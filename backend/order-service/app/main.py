c40f0f03afc1a34d43aee61186cbae13becfddce
Akash N <akashnatraj366@gmail.com>
2026-04-22 22:27:00 +0530

    Payment event created

--- backend/order-service/app/main.py
+++ backend/order-service/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from .database import engine, Base
 from .routes import orders
 
+
+import threading
+from .payment_consumer import start_payment_consumer
+
 app = FastAPI(title="Order Service")
 
 Base.metadata.create_all(bind=engine)
@@ -11,3 +15,10 @@ app.include_router(orders.router)
 @app.get("/")
 def root():
     return {"service": "Order Service Running"}
+
+def start_consumer():
+    thread = threading.Thread(target=start_payment_consumer)
+    thread.daemon = True
+    thread.start()
+
+start_consumer()
