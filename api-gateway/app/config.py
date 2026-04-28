16ed2985ea9d06e8fedcda7e4293e6b7cbf7d9a7
Akash N <akashnatraj366@gmail.com>
2026-04-28 14:35:12 +0530

    ..

---
+++ api-gateway/app/config.py
@@ -0,0 +1,12 @@
+SERVICES = {
+    "auth": "http://auth-service:8000",
+    "users": "http://user-service:8000",
+    "products": "http://product-service:8000",
+    "inventory": "http://inventory-service:8000",
+    "cart": "http://cart-service:8000",
+    "orders": "http://order-service:8000",
+    "payments": "http://payment-service:8000",
+    "delivery": "http://delivery-service:8000",
+    "notifications": "http://notification-service:8000",
+    "support": "http://support-service:8000",
+}
