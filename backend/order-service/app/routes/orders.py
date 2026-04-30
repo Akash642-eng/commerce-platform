554a7b027110c6790a7f8ab6a8351fd448c5b697
Akash N <akashnatraj366@gmail.com>
2026-04-30 12:38:40 +0530

    order

--- backend/order-service/app/routes/orders.py
+++ backend/order-service/app/routes/orders.py
@@ -1,15 +1,44 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models, schemas
-from ..rabbitmq_producer import publish_event
+import pika
+import json
+import os
 
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
 
+def publish_event(queue, data, trace_id):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBITMQ_HOST)
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue, durable=True)
+
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue,
+        body=json.dumps(data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+            headers={"x-trace-id": trace_id}   # ✅ ROOT TRACE
+        )
+    )
+
+    print(f"[TRACE {trace_id}] 🚀 Published {queue}: {data}", flush=True)
+
+    connection.close()
+
+
 @router.post("/")
-def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
+def create_order(order: schemas.OrderCreate, request: Request, db: Session = Depends(get_db)):
+
+    trace_id = request.headers.get("x-trace-id", "N/A")
+
     new_order = models.Order(
         user_id=order.user_id,
         total_amount=order.total_amount,
@@ -28,20 +57,13 @@ def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
         )
         db.add(order_item)
 
-    status = models.OrderStatusHistory(
-        order_id=new_order.id,
-        status="CREATED"
-    )
-    db.add(status)
-
     db.commit()
 
-    # Send event to RabbitMQ
     publish_event("order_created", {
         "order_id": new_order.id,
         "user_id": str(new_order.user_id),
         "amount": float(new_order.total_amount)
-    })
+    }, trace_id)
 
     return {"order_id": new_order.id, "status": "CREATED"}
 
