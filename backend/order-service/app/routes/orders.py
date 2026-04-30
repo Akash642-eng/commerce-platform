e3b5d6e3b2e51285ee2008b4c150d0a0e437697a
Akash N <akashnatraj366@gmail.com>
2026-04-30 20:18:42 +0530

    logging

--- backend/order-service/app/routes/orders.py
+++ backend/order-service/app/routes/orders.py
@@ -2,36 +2,10 @@ from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models, schemas
-import pika
-import json
-import os
-
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
+from ..rabbitmq_producer import publish_event
+from ..logger import log_event
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
-
-
-def publish_event(queue, data, trace_id):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=RABBITMQ_HOST)
-    )
-    channel = connection.channel()
-
-    channel.queue_declare(queue=queue, durable=True)
-
-    channel.basic_publish(
-        exchange='',
-        routing_key=queue,
-        body=json.dumps(data),
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-            headers={"x-trace-id": trace_id}   # ✅ ROOT TRACE
-        )
-    )
-
-    print(f"[TRACE {trace_id}] 🚀 Published {queue}: {data}", flush=True)
-
-    connection.close()
 
 
 @router.post("/")
@@ -59,6 +33,14 @@ def create_order(order: schemas.OrderCreate, request: Request, db: Session = Dep
 
     db.commit()
 
+    log_event(
+        "order-service",
+        trace_id,
+        "Order created",
+        {"order_id": new_order.id, "status": "CREATED"}
+    )
+
+    # send event
     publish_event("order_created", {
         "order_id": new_order.id,
         "user_id": str(new_order.user_id),
