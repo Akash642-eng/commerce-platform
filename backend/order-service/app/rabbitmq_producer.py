cc5c7576f721e77f2987ccadb59cd29a24bbd3b9
Akash N <akashnatraj366@gmail.com>
2026-04-30 20:18:30 +0530

    order logging

--- backend/order-service/app/rabbitmq_producer.py
+++ backend/order-service/app/rabbitmq_producer.py
@@ -1,22 +1,34 @@
 import pika
 import json
 import os
+import uuid
+
+from .logger import log_event
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
 
-def publish_event(queue, message):
+
+def publish_event(queue, data, trace_id=None):
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=RABBITMQ_HOST)
     )
-
     channel = connection.channel()
-    channel.queue_declare(queue="order_created", durable=True)
+
+    channel.queue_declare(queue=queue, durable=True)
+
+    if not trace_id:
+        trace_id = str(uuid.uuid4())  # ✅ NEW TRACE START
 
     channel.basic_publish(
         exchange='',
         routing_key=queue,
-        body=json.dumps(message),
-        properties=pika.BasicProperties(delivery_mode=2)
+        body=json.dumps(data),
+        properties=pika.BasicProperties(
+            delivery_mode=2,
+            headers={"x-trace-id": trace_id}
+        )
     )
+
+    log_event("order-service", trace_id, f"Published {queue}", data)
 
     connection.close()
