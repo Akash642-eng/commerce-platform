4e6d49ae3a0ae7ae17282d7cbea8d362c359569f
Akash N <akashnatraj366@gmail.com>
2026-05-09 14:48:50 +0530

    fix

--- backend/user-service/app/routes/auth.py
+++ backend/user-service/app/routes/auth.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
+from fastapi import status
 
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+
 from app import models
 from app import schemas
 
-from app.security import verify_password
 from app.security import create_access_token
+from app.security import verify_password
 
 
 router = APIRouter(
@@ -18,7 +20,13 @@ router = APIRouter(
 )
 
 
-@router.post("/login")
+# --------------------------------
+# LOGIN
+# --------------------------------
+@router.post(
+    "/login",
+    response_model=schemas.TokenResponse
+)
 def login(
     user_credentials: schemas.UserLogin,
     db: Session = Depends(get_db)
@@ -30,31 +38,48 @@ def login(
         models.User.email == user_credentials.email
     ).first()
 
+
+    # --------------------------------
+    # USER NOT FOUND
+    # --------------------------------
     if not user:
 
         raise HTTPException(
-            status_code=401,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password"
         )
 
+
+    # --------------------------------
+    # PASSWORD VALIDATION
+    # --------------------------------
     valid_password = verify_password(
         user_credentials.password,
-        user.password
+        user.hashed_password
     )
 
     if not valid_password:
 
         raise HTTPException(
-            status_code=401,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password"
         )
 
+
+    # --------------------------------
+    # CREATE JWT TOKEN
+    # --------------------------------
     access_token = create_access_token(
         data={
+            "sub": user.email,
             "user_id": user.id
         }
     )
 
+
+    # --------------------------------
+    # RESPONSE
+    # --------------------------------
     return {
         "access_token": access_token,
         "token_type": "bearer"
