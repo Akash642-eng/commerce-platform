e34a93a6299c12043505a2ad963297012396d0c9
Akash N <akashnatraj366@gmail.com>
2026-05-12 18:43:50 +0530

    Fix

--- services/order_service/app/config.py
+++ services/order_service/app/config.py
@@ -2,22 +2,33 @@ import os
 
 
 class Settings:
-    def __init__(self):
-       
-        self.ENV = os.getenv("ENV", "dev")
 
-       #db
+    def __init__(self):
+
+        self.ENV = os.getenv(
+            "ENV",
+            "dev"
+        )
+
         self.DATABASE_URL = os.getenv(
             "DATABASE_URL",
             "postgresql://commerce:commerce@postgres:5432/commerce_db"
         )
 
-       
-        self.RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-        
-        self.REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+        self.RABBITMQ_HOST = os.getenv(
+            "RABBITMQ_HOST",
+            "rabbitmq"
+        )
 
-        self.JWT_SECRET = os.getenv("JWT_SECRET", "dev-secret")
+        self.REDIS_HOST = os.getenv(
+            "REDIS_HOST",
+            "redis"
+        )
+
+        self.JWT_SECRET = os.getenv(
+            "JWT_SECRET",
+            "dev-secret"
+        )
 
 
 settings = Settings()
