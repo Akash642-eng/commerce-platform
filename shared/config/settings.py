acab6361520a7273cc525caf48cd4679d3763448
Akash N <akashnatraj366@gmail.com>
2026-05-30 19:41:23 +0530

    Updated db url

--- shared/config/settings.py
+++ shared/config/settings.py
@@ -7,36 +7,27 @@ load_dotenv()
 
 class Settings:
 
-    # --------------------------------
     # ENVIRONMENT
-    # --------------------------------
     ENV = os.getenv("ENV", "dev")
 
-    # --------------------------------
     # DATABASE
-    # --------------------------------
     DATABASE_URL = os.getenv(
         "DATABASE_URL",
         "postgresql://commerce:commerce@postgres.database.svc.cluster.local:5432/commerce_db",
     )
 
-    # --------------------------------
     # REDIS
-    # --------------------------------
     REDIS_HOST = os.getenv("REDIS_HOST", "redis.cache.svc.cluster.local")
 
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
-    # --------------------------------
     # RABBITMQ
-    # --------------------------------
+
     RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq.messaging.svc.cluster.local")
 
     RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
 
-    # --------------------------------
     # JWT
-    # --------------------------------
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
     ALGORITHM = os.getenv("ALGORITHM", "HS256")
