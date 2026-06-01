bae5514e3ac3c3f1ec931ae85ce5924ba7c77f37
Akash N <akashnatraj366@gmail.com>
2026-06-01 20:50:26 +0530

    Added centralized timeout config

--- shared/resilience/config.py
+++ shared/resilience/config.py
@@ -1,2 +1,9 @@
-HTTP_TIMEOUT = 5
-DB_TIMEOUT = 10
+import os
+
+HTTP_TIMEOUT = int(os.getenv("HTTP_TIMEOUT", 5))
+
+DB_TIMEOUT = int(os.getenv("DB_TIMEOUT", 10))
+
+RABBITMQ_TIMEOUT = int(os.getenv("RABBITMQ_TIMEOUT", 5))
+
+EXTERNAL_API_TIMEOUT = int(os.getenv("EXTERNAL_API_TIMEOUT", 10))
